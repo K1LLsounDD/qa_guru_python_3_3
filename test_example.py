584b05ed496808bf0d34a78d3493cfbaf50c54b6
K1LLsounD <sgol99@mail.ru>
2022-11-26 20:33:07 +0300

    added test_example

--- test_example.py
+++ test_example.py
@@ -1,14 +1,8 @@
 def test_example_1():
-    assert 6 > 4
-
+    assert 5 > 4
+    assert 4 == 4
 
 
 def test_example_3():
-    assert 5 > 4
-    assert 0 < 4
-
-
-def test_example_4():
-    assert 0 != 3
-    assert 0 == 0
-
+    assert 7 > 4
+    assert 3 < 4
