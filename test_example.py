1d06eaeef0a26d9c888695279217c3e1277e8460
K1LLsounD <sgol99@mail.ru>
2022-11-26 19:56:51 +0300

    test_example

---
+++ test_example.py
@@ -0,0 +1,14 @@
+def test_example_1():
+    assert 6 > 4
+    assert 5 < 6
+
+
+def test_example_2():
+    assert 6 > 4
+    assert 1 < 4
+
+
+def test_example_3():
+    assert 0 != 4
+    assert 0 == 0
+
